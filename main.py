fcf9fb0b22dce06bc22c760435eef37929901c37
TaqiyEddine Bouklouha <25593465+TaqiyEddine-B@users.noreply.github.com>
2024-03-03 08:40:12 +0100

    Add OpenAI Key input and dotenv support

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 """ Main file for the project"""
+import os
+
 import streamlit as st
+
 from generator import Generator
+from dotenv import load_dotenv
+load_dotenv()
+
+
+
 
 st.set_page_config(page_title="Interview Prep App", page_icon=":sunglasses:", layout="wide", initial_sidebar_state="expanded")
 
 st.title("Interview Preparation App with Streamlit")
 st.write("This is a simple app to help you prepare for your next interview. It will compare your CV with the job description and give you a list of questions to prepare for.")
+
+
+openai_key = st.sidebar.text_input('OpenAI Key', '')
+if len(openai_key) > 0:
+    os.environ["OPENAI_API_KEY"] = openai_key
+else:
+    if 'OPENAI_API_KEY' not in os.environ:
+        st.error('No OpenAI Key')
+        st.stop()
+    else:
+        st.toast('Using OpenAI Key from .env')
+
 # read the md file
 with open("data/cv.md", "r") as file:
     cv_desc = file.read()
